b229281aeeb5f02c7500024c906db12647178958
Vladimir Selivanov <recommendation082@gmail.com>
2023-09-02 22:37:52 +0500

    added

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Template from './Template.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Template />
+  </React.StrictMode>,
+)
---
+++ src/Template.tsx
@@ -0,0 +1,12 @@
+
+import styles from './Template.module.css'
+
+function Template() {
+  return (
+    <div className={styles.test}>
+        <p>123</p>
+    </div>
+  )
+}
+
+export default Template;
